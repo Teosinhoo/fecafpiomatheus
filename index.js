d25dcb5bfc1aa8db594310634c17147c4ed933d4
unknown <henrriquematheus21@gmail.com>
2021-05-30 16:21:37 -0300

    PIO3

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const express = require ('express');
+const path = require ('path');
+const config = require('config');
+
+const port = process.env.PORT || config.get('server.port');
+
+const app = express();
+
+app.set('port', port);
+
+app.route('/')
+
 console.log("iniciado")
