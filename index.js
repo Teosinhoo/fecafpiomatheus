4dd35c7f4b6be385ad9c53dc0331f71b4d1383a6
unknown <henrriquematheus21@gmail.com>
2021-05-30 16:25:27 -0300

    PIO3

--- index.js
+++ index.js
@@ -3,11 +3,24 @@ const path = require ('path');
 const config = require('config');
 
 const port = process.env.PORT || config.get('server.port');
+const pedidoAtual = {
+    cliente:"", 
+    barbeiro: "", 
+    endereço:"", 
+    horario:"",
+},
 
 const app = express();
 
 app.set('port', port);
 
-app.route('/')
+app.route('/Agendamentos').get(
+    (req, res) => {
+        res.status(200).json(pedidoAtual);
+    }
+)
 
+app.listen(port, () => {
+    ("Servidor iniciado na porta " + port);
+})
 console.log("iniciado")
